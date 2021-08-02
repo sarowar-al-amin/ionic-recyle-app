af725c179c9d1ca866085cec9d63acf6ed837b8d
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2021-08-02 13:38:53 +0600

    Create all the negivation path according to the design

--- src/app/pages/pickup-call/pickup-call.page.spec.ts
+++ src/app/pages/pickup-call/pickup-call.page.spec.ts
@@ -1,19 +1,26 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { PickupCallPage } from './pickup-call.page';
 
 describe('PickupCallPage', () => {
   let component: PickupCallPage;
   let fixture: ComponentFixture<PickupCallPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PickupCallPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(PickupCallPage);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
@@ -21,4 +28,11 @@ describe('PickupCallPage', () => {
   // it('should create', () => {
   //   expect(component).toBeTruthy();
   // });
+
+  // Naviagte to home page
+  it(`It should redirect to home page`, ()=>{
+    spyOn(router, 'navigate');
+    component.newPickupCall();
+    expect(router.navigate).toHaveBeenCalledWith(['home']);
+  });
 });
